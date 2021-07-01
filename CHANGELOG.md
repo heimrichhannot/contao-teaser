4946bc55b9308618dd29fffd49a7a20136498cbf
Dennis Patzer <d.patzer@heimrich-hannot.de>
2021-07-01 11:50:18 +0200

    see cl 1.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
+
 All notable changes to this project will be documented in this file.
+
+## [1.2.0] - 2021-07-01
+
+- added Polish translation
 
 ## [1.1.1] - 2020-09-18
 
@@ -8,32 +13,40 @@ All notable changes to this project will be documented in this file.
 ## [1.1.0] - 2018-08-27
 
 ### Added
+
 - support for hypergalaktisch/contao-animate
 
 ## [1.0.14] - 2018-08-15
 
 ### Added
-- notice to readme to use the [teaser bundle](https://github.com/heimrichhannot/contao-teaser-bundle) if contao 4 is used.
+
+- notice to readme to use the [teaser bundle](https://github.com/heimrichhannot/contao-teaser-bundle) if contao 4 is
+  used.
 
 ## [1.0.13] - 2018-01-24
 
 ### Added
+
 - english translation
 
 ### Changed
+
 - licence LGPL-3.0+ is now LGPL-3.0-or-later
 
 ## [1.0.12] - 2017-06-08
 
 ### Added
+
 - support for php 7 and contao 4
 
 ## [1.0.11] - 2017-04-28
 
 ### Changed
+
 - varchar lengths to reduce db size
 
 ## [1.0.10] - 2017-02-28
 
 ### Added
+
 - add active and trail css class for active/trail pages
---
+++ languages/pl/default.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Teaser links
+ */
+$GLOBALS['TL_LANG']['MSC']['linkteaser']['teaserlinktext']['default'][0]           = 'więcej';
+$GLOBALS['TL_LANG']['MSC']['linkteaser']['teaserlinktext']['download'][0]          = 'Do pobrania';
+$GLOBALS['TL_LANG']['MSC']['linkteaser']['teaserlinktext']['pageWithTitle'][0]     = 'Odwiedź stronę "%s"';
+$GLOBALS['TL_LANG']['MSC']['linkteaser']['teaserlinktext']['fileWithTitle'][0]     = 'Pokaż stronę "%s”';
+$GLOBALS['TL_LANG']['MSC']['linkteaser']['teaserlinktext']['downloadWithTitle'][0] = 'Pobierz stronę "%s”';
+$GLOBALS['TL_LANG']['MSC']['linkteaser']['teaserlinktext']['articleWithTitle'][0]  = 'Szukaj artykułu "%s"';
+$GLOBALS['TL_LANG']['MSC']['linkteaser']['teaserlinktext']['externalMailTitle'][0] = 'Napisz wiadomość e-mail do "%s"';
+$GLOBALS['TL_LANG']['MSC']['linkteaser']['teaserlinktext']['externalLinkTitle'][0] = 'Odwiedź zewnętrzną stronę "%s"';
+
+$GLOBALS['TL_LANG']['MSC']['linkteaser']['pageTitle']         = 'Odwiedź stronę: %s';
+$GLOBALS['TL_LANG']['MSC']['linkteaser']['fileTitle']         = 'Pokaż plik: %s';
+$GLOBALS['TL_LANG']['MSC']['linkteaser']['downloadTitle']     = 'Pobierz plik: %s';
+$GLOBALS['TL_LANG']['MSC']['linkteaser']['articleTitle']      = 'Pokaż artykuł: %s';
+$GLOBALS['TL_LANG']['MSC']['linkteaser']['externalMailTitle'] = 'Napisz wiadomość e-mail do: %s';
+$GLOBALS['TL_LANG']['MSC']['linkteaser']['externalLinkTitle'] = 'Odwiedź stronę: %s';
+
+
+/**
+ * Content elements
+ */
+$GLOBALS['TL_LANG']['CTE']['linkteaser'] = ['Link-Teaser', 'Tworzy linkowany teaser do strony, artykułu, pliku lub zewnętrznego adresu URL.'];
